b7b676018b5d7d030ac8436d8aa5d3679e58a4f0
FrankValdezQuedo <frank24111999@gmail.com>
2026-05-21 10:50:35 -0500

    primer commit

---
+++ src/main/java/com/pratica/crud_springbboot/CrudSpringbbootApplication.java
@@ -0,0 +1,13 @@
+package com.pratica.crud_springbboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringbbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringbbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratica/crud_springbboot/CrudSpringbbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratica.crud_springbboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringbbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
